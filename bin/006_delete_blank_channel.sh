d1a39b77db86b1ee8af76591eaff58aed4e73944
yamasita <yamasita.taisuke@bpsinc.jp>
2015-09-08 23:49:36 +0900

    update

--- bin/006_delete_blank_channel.sh
+++ bin/006_delete_blank_channel.sh
@@ -2,4 +2,5 @@
 set -e
 
 cd tmp
-grep -F -l '"messages": [],' *  | xargs rm
+#grep -F -l '"messages": [],' *  | xargs rm
+find . -size -200c ! -name .gitignore | xargs --no-run-if-empty rm
--- conf/token.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-API_TOKEN=xoxp-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-MAIL_FROM=from@example.com
-MAIL_TO=to@example.com
